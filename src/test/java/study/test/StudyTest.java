a146f0506b36d214e19189a4bb3fee69972145ac
wandol <seok45861@gmail.com>
2020-06-22 00:03:52 +0900

    테스트 이름 표기하

--- src/test/java/study/test/StudyTest.java
+++ src/test/java/study/test/StudyTest.java
@@ -4,18 +4,22 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
-
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class StudyTest {
 
 	@Test
-	void create() {
+	@DisplayName("첫 테스트 생성")
+	void create_new_one() {
 		System.out.println("create");
 	}
 	
 	@Test
-	void create1() {
+	void create_new_two() {
 		System.out.println("create 1");
 	}
 	
